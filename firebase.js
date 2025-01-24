a16318f758651ddc93dfd4fc1736d6702ed2d6e3
Nelson Orina <orinamecha2@gmail.com>
2025-01-24 15:11:53 +0300

    Notification global

--- firebase.js
+++ firebase.js
@@ -1,21 +1,31 @@
 import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging";
 
+// Declare and assign environment variables
+const apiKey = import.meta.env.VITE_FIREBASE_API;
+const authDomain = import.meta.env.VITE_AUTHDOMAIN;
+const projectId = import.meta.env.VITE_PROJECT_ID;
+const storageBucket = import.meta.env.VITE_STORAGE_BUCKET;
+const messagingSenderId = import.meta.env.VITE_MESSAGINGSENDERID;
+const appId = import.meta.env.VITE_APP_ID;
+const measurementId = import.meta.env.VITE_MEASUREMENT_ID;
+
+// Firebase configuration object
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API,
-    authDomain: import.meta.env.VITE_AUTHDOMAIN,
-    projectId: import.meta.env.VITE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-    appId: import.meta.env.VITE_APP_ID,
-    measurementId: import.meta.env.VITE_MEASUREMENT_ID
+    apiKey: apiKey,
+    authDomain: authDomain,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId,
+    appId: appId,
+    measurementId: measurementId,
 };
 
-console.log(apiKey);
-  
-
+// Initialize Firebase app
 const firebaseApp = initializeApp(firebaseConfig);
 
+// Initialize Firebase Cloud Messaging
 export const messaging = getMessaging(firebaseApp);
 
+// Export the initialized Firebase app
 export default firebaseApp;
