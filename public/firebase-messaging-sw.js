ca65e8048f4509652a4f547b2628920e5e3a65e1
Nelson Orina <orinamecha2@gmail.com>
2025-01-27 17:29:11 +0300

    Firebase notification

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,16 +1,9 @@
 // firebase-messaging-sw.js
+importScripts('/src/firebase-config.js');
+
 importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAnVi5xKuQ28khhmZHxthf0q6cN9IYuEyA",
-    authDomain: "swyft-38122.firebaseapp.com",
-    projectId: "swyft-38122",
-    storageBucket: "swyft-38122.firebasestorage.app",
-    messagingSenderId: "673548770885",
-    appId: "1:673548770885:web:70ab19c1a0f60a0e550d2c",
-    measurementId: "G-MRL2YDHJPR"
-};
 
 firebase.initializeApp(firebaseConfig);
 
