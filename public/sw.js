a38613a2de476ade048b4e936d83c39b944c8cdf
Nelson Orina <orinamecha2@gmail.com>
2025-02-12 09:12:26 +0300

    Making driver app a PWA

---
+++ public/sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "pwa-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/app.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
+];
+
+// Install Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate & Clean Old Caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch and Cache Strategy
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
